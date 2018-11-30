8fbcbcd4e73c8f9b4177dc3084af30d138637bd9
Neulana <fupinyou@gmail.com>
2018-11-30 23:58:02 +0800

    ✨ remove unuse code

--- src/background.js
+++ src/background.js
@@ -11,41 +11,21 @@ chrome.runtime.onConnect.addListener(function (port) {
             xhr.onload = function () {
                 console.log(xhr);
                 if (xhr.status == 200) {
-                    msg.success = true;
-                    port.postMessage(msg);
-                    cached[msg.url] = msg;
-                } else if (xhr.status == 404) {
-                    msg.success = false;
-                    port.postMessage(msg);
-                    cached[msg.url] = msg;
-                }
-            };
-            xhr.open('GET', msg.url, true);
-            xhr.setRequestHeader('Content-type', 'text/html');
-            xhr.send();
-        });
-    } else if (port.name == "neets-search") {
-        port.onMessage.addListener(function (msg) {
-            if (cached[msg.url]) {
-                port.postMessage(cached[msg.url]);
-                return;
-            }
-            var xhr = new XMLHttpRequest();
-            xhr.onload = function () {
-                console.log(xhr);
-                if (xhr.status == 200) {
                     var dom = document.createElement('html');
                     dom.innerHTML = xhr.responseText;
                     var resoures = dom.getElementsByClassName('themes_list_cen_li2');
                     if (resoures) {
+                        msg.neetLinks = [];
                         for (var i = 0; i < resoures.length; i++) {
-                            var details = resoures[i].split("\n");
-                            msg.movie_title = details[0];
-                            msg.movie_url = details[1];
-                            msg.success = true;
-                            port.postMessage(msg);
-                            cached[msg.url] = msg;
+                            var movieTitle = resoures[i].getElementsByClassName('tit')[0].nodeValue;
+                            var movieUrl = resoures[i].getElementsByClassName('themes_a')[0].nodeValue;
+                            console.log('movieTitle: ' + movieTitle);
+                            console.log('movieUrl: ' + movieUrl);
+                            msg.neetLinks.push([movieTitle, movieUrl]);
                         }
+                        msg.success = true;
+                        port.postMessage(msg);
+                        cached[msg.url] = msg;
                     } else {
                         msg.success = false;
                         port.postMessage(msg);
--- src/core.js
+++ src/core.js
@@ -1,15 +1,11 @@
 (function () {
-    var gb = {
-        LINK_TYPE: {
-            SUBJECT: 0
-        }
-    }
 
     var port = null;
     var dict = {};
+    var repoAddr = 'https://github.com/Neulana/douban-movie-extension';
 
     // search with movie name
-    function search(movieName, linkType, parentTag) {
+    function search(movieName, parentTag) {
         if (!port || port.name != 'douban-movie') {
             port = chrome.runtime.connect({
                 name: 'douban-movie'
@@ -24,9 +20,8 @@
                 var task;
                 while (dict[url].tasks.length > 0) {
                     task = dict[url].tasks.pop();
-                    var panel = getLinkStyle(task.linkType, url);
-                    task.parentTag.appendChild(panel);
-                    task.parentTag.setAttribute('has-neets', '1');
+                    var panel = getLinkStyle(neetLinks);
+                    task.parentTag.insertBefore(panel, task.parentTag.childNodes[0]);
                 }
             });
         }
@@ -37,12 +32,10 @@
             };
         }
         if (dict[url].found) {
-            var panel = getLinkStyle(linkType, url);
-            parentTag.appendChild(panel);
-            parentTag.setAttribute('has-neets', '1');
+            // var panel = getLinkStyle(url);
+            // parentTag.appendChild(panel);
         } else if (dict[url].found == undefined) {
             dict[url].tasks.push({
-                linkType: linkType,
                 parentTag: parentTag
             })
             port.postMessage({
@@ -51,69 +44,40 @@
         }
     }
 
-    function getLinkStyle(linkType, readfreeUrl) {
-        var className = null;
-        var text = null;
-        if (linkType === gb.LINK_TYPE.SUBJECT) {
-            className = 'rf-book-page-main-link';
-            text = 'Neets!';
+    function getLinkStyle(neetLinks) {
+        var neetsDiv = document.createElement('div');
+        var neetsH2 = document.createElement('h2');
+        var starMe = document.createElement('a');
+        neetsDiv.setAttribute('class', 'gray_ad');
+        neetsH2.innerText = '豆瓣电影传送门&nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·'
+        starMe.setAttribute('href', repoAddr);
+        starMe.setAttribute('target', '_blank');
+        starMe.setAttribute('class', 'report-error');
+        starMe.innerText = 'StarMe!';
+        neetsDiv.appendChild(neetsH2);
+        neetsDiv.appendChild(starMe);
+        var neetsUl = document.createElement('ul');
+        neetsUl.setAttribute('class', 'bs');
+        for (var i = 0; i < neetLinks.length; i++) {
+            var neetsLi = document.createElement('li');
+            var neetsMovie = document.createElement('a');
+            neetsMovie.setAttribute('href', neetLinks[i][1]);
+            neetsMovie.setAttribute('target', '_blank');
+            neetsMovie.setAttribute('class', 'playBtn');
+            neetsMovie.innerHTML = neetLinks[i][0];
+            neetsLi.appendChild(neetsMovie);
+            neetsUl.appendChild(neetsLi);
         }
-        if (className !== null) {
-            var ahref = document.createElement('a');
-            ahref.setAttribute('href', readfreeUrl);
-            ahref.setAttribute('target', '_blank');
-            ahref.setAttribute('class', className);
-            ahref.innerHTML = text;
-            return ahref;
-        }
-        return null;
-    }
-
-
-    function insertCss() {
-        // insert css
-        var style = document.createElement('style');
-        // webkit hack
-        style.appendChild(document.createTextNode(''));
-        // insert to head
-        document.head.appendChild(style);
-
-        // rules
-        var primaryColor = '#37a';
-        var secondaryColor = '#614e3c';
-        var rules = {
-            '.rf-book-page-main-link': {
-                position: 'fixed',
-                top: '160px',
-                left: '-10px',
-                padding: '10px 20px 10px 30px',
-                display: 'block',
-                background: secondaryColor,
-                color: 'white !important'
-            }
-        };
-        for (var ele in rules) {
-            var rulesStr = ele + '{';
-            for (var attr in rules[ele]) {
-                rulesStr += attr + ': ' + rules[ele][attr] + ';';
-            }
-            rulesStr += '} ';
-            style.sheet.insertRule(rulesStr, 0);
-        }
+        neetsDiv.appendChild(neetsUl);
+        return neetsDiv;
     }
 
     function runDouban() {
-        var pathRe = location.pathname.match(/\/(\w+)\/(\d+)\//);
-
-        // loadDoubanReadfree();
-
-        // book page
-        if (pathRe) {
-            var urlClass = pathRe[1];
-            var doubanId = pathRe[2];
-            if (urlClass === 'subject') {
-                search(doubanId, gb.LINK_TYPE.SUBJECT, document.body);
-            }
+        var h1 = document.getElementsByTagName('h1')[0];
+        var movieName = h1.firstElementChild.innerText;
+        var aside = document.getElementsByClassName('aside');
+        if (aside) {
+            search(movieName, aside[0]);
         }
 
         // douban book
@@ -135,54 +99,8 @@
                 }, false);
             }
         }
-
-        // search neets when load more
-        reloadIndex(['a_nointerest_subject', 'load-more', 'book_x'], function () {
-            setTimeout(function () {
-                loadDoubanReadfree();
-            }, 2000);
-        });
-
-        function loadDoubanReadfree() {
-            var links = document.getElementsByTagName('a');
-            for (var i in links) {
-                (function (e) {
-                    // ignore those with loaded neets
-                    if (typeof links[e] === 'object' &&
-                        links[e].parentNode.getAttribute(
-                            'has-neets') === '1') {
-                        return;
-                    }
-
-                    var re = links[e].href === undefined ? null :
-                        links[e].href.match(/\/subject\/(\d+)(\/$|\/\?)/);
-
-                    // ignore those with both title and images
-                    if (links[e].className === 'cover') {
-                        // cover image in people page, don't ignore
-                        search(re[1], gb.LINK_TYPE.IMAGE, links[e].parentNode);
-                        return;
-                    }
-                })(i);
-            }
-        }
     }
 
-    // index page of read
-    function reloadIndex(classNameList, callback) {
-        for (var cid = 0, clen = classNameList.length; cid < clen; ++cid) {
-            var bookX = document.getElementsByClassName(classNameList[cid]);
-            if (bookX.length > 0 && callback) {
-                for (var i = 0; i < bookX.length; ++i) {
-                    bookX[i].addEventListener('click', function () {
-                        callback();
-                    });
-                }
-            }
-        }
-    }
-
-    insertCss();
     var host = window.location.hostname;
     if (host === 'movie.douban.com') {
         runDouban();
