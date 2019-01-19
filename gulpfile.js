2e041a40caeb75a54ec0d77984e66396f72f806c
Neulana <fupinyou@gmail.com>
2019-01-19 20:52:33 +0800

    modify gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,9 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-var pump = require('pump');
-var concat = require('gulp-concat');
-
-gulp.task('default', ['compress'], function() {
-    gulp.watch('src/core.js', ['default']);
-    return gulp.src(['tmp/core.js',  'tmp/background.js'])
-        .pipe(gulp.dest('build'));
-});
 
 gulp.task('compress', function () {
     return gulp.src(['src/core.js', 'src/background.js'])
         .pipe(uglify())
-        .pipe(gulp.dest('tmp'));
+        .pipe(gulp.dest('tmp'))
+        .pipe(gulp.dest('build'));
 });
